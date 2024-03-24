6e73709028a218f0d6e26e635e13066a4cef4ccb
hendisantika <hendisantika@yahoo.co.id>
2024-03-24 08:01:14 +0700

    Add PersistenceConfig 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootmybatissample2/config/PersistenceConfig.java
@@ -0,0 +1,41 @@
+package id.my.hendisantika.springbootmybatissample2.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-mybatis-sample2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/24/24
+ * Time: 08:00
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+//@MapperScan("id.my.hendisantika.springbootmybatissample2.mapper")
+public class PersistenceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("schema.sql")
+                .addScript("data.sql")
+                .build();
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource());
+        return factoryBean.getObject();
+    }
+}
