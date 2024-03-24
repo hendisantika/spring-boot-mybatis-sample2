11ab4819ffe05331d240f87d58d2ae45341b889d
hendisantika <hendisantika@yahoo.co.id>
2024-03-24 07:57:51 +0700

    Create starter project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootmybatissample2/SpringBootMybatisSample2Application.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootmybatissample2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisSample2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMybatisSample2Application.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springbootmybatissample2/SpringBootMybatisSample2ApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootmybatissample2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMybatisSample2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
