e3ef20769920ba5ec9898a580d98ad45d2192751
hendisantika <hendisantika@yahoo.co.id>
2024-03-24 07:59:52 +0700

    Add schema.sql file 🫸🌀✏️📗🐧🐳

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE if NOT EXISTS `TBL_TODO`
+(
+    `id`
+    INTEGER
+    PRIMARY
+    KEY,
+    `title`
+    VARCHAR
+(
+    100
+) NOT NULL,
+    `body` VARCHAR
+(
+    2000
+) NOT NULL
+    );
