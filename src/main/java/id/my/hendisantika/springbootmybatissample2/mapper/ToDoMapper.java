1f526aefea455559351dbf8ecf67bfbe0cc51d19
hendisantika <hendisantika@yahoo.co.id>
2024-03-24 08:01:47 +0700

    Add ToDoMapper 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootmybatissample2/mapper/ToDoMapper.java
@@ -0,0 +1,41 @@
+package id.my.hendisantika.springbootmybatissample2.mapper;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-mybatis-sample2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/24/24
+ * Time: 08:01
+ * To change this template use File | Settings | File Templates.
+ */
+@Mapper
+public interface ToDoMapper {
+
+    @Select("SELECT * FROM tbl_todo")
+    List<TODO> findAll();
+
+    @Select("SELECT * FROM tbl_todo WHERE id = #{id}")
+    TODO findById(@Param("id") Long id);
+
+    @Delete("DELETE FROM tbl_todo WHERE id = #{id}")
+    int deleteById(@Param("id") Long id);
+
+    @Insert("INSERT INTO tbl_todo(id, title, body) " +
+            " VALUES (#{id}, #{title}, #{body})")
+    int createNew(TODO item);
+
+    @Update("UPDATE tbl_todo SET title=#{title}, " +
+            " body=#{body} WHERE id=#{id}")
+    int update(TODO item);
+}
